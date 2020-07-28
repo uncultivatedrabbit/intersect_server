dcdfc0e9f7ce470eb495085803034893162d8126
Brandon Melendez <brandonmelendezdev@gmail.com>
2020-07-28 16:20:41 -0400

    first commit

---
+++ src/app.js
@@ -0,0 +1,31 @@
+require("dotenv").config();
+const express = require("express");
+const morgan = require("morgan");
+const cors = require("cors");
+const helmet = require("helmet");
+const { NODE_ENV } = require("./config");
+
+const app = express();
+
+const morganOptions = NODE_ENV === "production" ? "tiny" : "common";
+
+app.use(morgan(morganOptions));
+app.use(helmet());
+app.use(cors());
+
+app.get("/", (req, res) => {
+  res.send("Hello, world!");
+});
+
+app.use(function errorHandler(error, req, res, next) {
+  let response;
+  if (NODE_ENV === "production") {
+    response = { error: "Server error" };
+  } else {
+    console.error(error);
+    response = { error: error.message, object: error };
+  }
+  res.status(500).json(response);
+});
+
+module.exports = app;
